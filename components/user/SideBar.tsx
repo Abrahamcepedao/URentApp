2332248d6d55b63680bffadf27f18e9813db6fa1
Abrahamcepedao <a00827666@itesm.mx>
2023-01-24 19:29:11 -0600

    Added tenant and contract information form

--- components/user/SideBar.tsx
+++ components/user/SideBar.tsx
@@ -42,7 +42,7 @@ const SideBar = () => {
                 </Link>
                 <Link className={styles.tab} href="/properties">
                     <MapsHomeWorkRoundedIcon className={styles.tab__icon}/>
-                    <p className={styles.tab__label} style={{opacity: router.pathname === '/properies' ? 1 : 0.5}}>Propiedades</p>
+                    <p className={styles.tab__label} style={{opacity: router.pathname === '/properties' ? 1 : 0.5}}>Propiedades</p>
                 </Link>
                 <Link className={styles.tab} href="/users">
                     <PeopleRoundedIcon className={styles.tab__icon}/>
--- pages/properties.tsx
+++ pages/properties.tsx
@@ -16,6 +16,9 @@ import Chip from '../components/user/Chip'
 
 //Material UI
 import { Tooltip, IconButton, Collapse } from '@mui/material'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
 
 //Material UI - icons
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
@@ -23,6 +26,7 @@ import FilterAltRoundedIcon from '@mui/icons-material/FilterAltRounded';
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import Settings from '@mui/icons-material/Settings';
 
 const temp = [
   {
@@ -197,7 +201,15 @@ const temp = [
 
 //Dashboard page
 const Dashboard: NextPage = () => {
-  
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event:any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div>
@@ -228,7 +240,12 @@ const Dashboard: NextPage = () => {
 
                 {/*  */}
                 <Tooltip title="Filtrar" placement='top'>
-                  <IconButton>
+                  <IconButton
+                    onClick={(e) => {handleClick(e)}}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                  >
                     <FilterAltRoundedIcon className={dash.header__icon}/>
                   </IconButton>
                 </Tooltip>
@@ -304,6 +321,52 @@ const Dashboard: NextPage = () => {
           </div>
         </div>
       </main>
+
+      {/* Menu */}
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >
+        
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+       
+      </Menu>
     </div>
   )
 }
