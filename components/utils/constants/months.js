15168cd6967cdf2540491a9936b780faed2cacd8
Abrahamcepedao <a00827666@itesm.mx>
2023-01-30 12:55:14 -0600

    Added form to regisster payment

---
+++ components/utils/constants/months.js
@@ -0,0 +1,48 @@
+const months = [
+    {
+        en: "January",
+        es: "Enero"
+    },
+    {
+        en: "February",
+        es: "Febrero"
+    },
+    {
+        en: "March",
+        es: "Marzo"
+    },
+    {
+        en: "April",
+        es: "Abril"
+    },
+    {
+        en: "June",
+        es: "Junio"
+    },
+    {
+        en: "July",
+        es: "Julio"
+    },
+    {
+        en: "August",
+        es: "Agosto"
+    },
+    {
+        en: "September",
+        es: "Septiembre"
+    },
+    {
+        en: "October",
+        es: "Octubre"
+    },
+    {
+        en: "November",
+        es: "Noviembre"
+    },
+    {
+        en: "December",
+        es: "Diciembre"
+    }
+]
+
+export default months
